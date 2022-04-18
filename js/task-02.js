ef5cec65e21aab3a9ca6684c69a8384810eb2f19
Tetiana0310 <tetiana.posmashna@gmail.com>
2022-04-18 22:27:11 +0300

    Update task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector("#ingredients");
+
+const separateIngredients = ingredients => {
+  return ingredients.map(ingredient => {
+    const liItem = document.createElement("li");
+
+    liItem.textContent = ingredient;
+
+    liItem.classList.add("item");
+
+    return liItem;
+  })
+}
+const newIngredients = separateIngredients(ingredients)
+listIngredients.append(...newIngredients)
