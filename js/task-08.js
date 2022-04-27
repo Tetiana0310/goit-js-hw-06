8cfafaac243a6d8efdbbef0a80221c58a4d454de
Tetiana0310 <tetiana.posmashna@gmail.com>
2022-04-27 23:12:10 +0300

    Updated task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault()
+    // console.log(event)
+
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+        alert('Все поля должны быть заполнены');
+    } else {
+        console.log({ Email: email.value, Password: password.value });
+        event.currentTarget.reset();
+    }
+}
+
